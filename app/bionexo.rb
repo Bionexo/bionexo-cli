36cc2b0ea9377fb7b677f1f79cce920cdaa3b450
Lucas Camargo <lucascamargodealmeida@gmail.com>
2015-04-25 23:26:36 -0300

    creating quote_creator

--- app/bionexo.rb
+++ app/bionexo.rb
@@ -4,25 +4,21 @@ class Bionexo < Thor
 
   desc 'new', 'creates a new quotation'
   def new
-    Quotation.create
+    QuoteCreator.new.create_or_continue
   end
 
   desc 'continue', 'continue quotation creation'
   def continue(quotation_id)
-    puts Quotation.find quotation_id
+    quotation = Quotation.find quotation_id
+    QuoteCreator.new(quotation).create_or_continue
   end
 
   desc 'list', 'lists quotations'
-  def list
-    unless Quotation.initialized.exists?
-      puts 'No quotations found'
-      return
-    end
+  def list(status = :completed)
+    quotations = Quotation.where(status: status)
+    return puts 'No quotations found' if quotations.empty?
 
-    quotations = Quotation.initialized
-    quotations_count = quotations.size
-
-    puts "Found #{quotations_count} #{pluralize(quotations_count, 'offer')}"
+    puts "Found #{quotations.size} #{pluralize(quotations.size, 'offer')}"
     puts quotations
   end
 
---
+++ app/quote_creator.rb
@@ -0,0 +1,46 @@
+class QuoteCreator
+
+  EDITABLE_ATTRS = %w(title address email phone_number product_name product_quantity quotation_type)
+
+  def initialize(quotation = Quotation.new)
+    if quotation.new_record?
+      quotation.save(validate: false)
+      puts "Starting with new quotation #{quotation.id}"
+    else
+      puts "Continuing with #{quotation.id}"
+    end
+    @quotation = quotation
+  end
+
+  def create_or_continue
+    @quotation.edit!
+
+    editable_attributes.each { |attribute| update(attribute) }
+    @quotation.complete!
+
+    puts "Great job! Listing #{@quotation.id} is complete!"
+  end
+
+  private
+
+  def editable_attributes
+    EDITABLE_ATTRS.reject do |attr|
+      @quotation.send(attr).present?
+    end
+  end
+
+  def update(attribute)
+    print "#{attribute.humanize}: "
+    #@quotation.update_attribute(attribute, user_input)
+    @quotation.update_attribute(attribute, user_input)
+
+    raise if @quotation.invalid?(attribute)
+  rescue
+    puts "Error: #{@quotation.attribute_error(attribute)}"
+    user_input and abort
+  end
+
+  def user_input
+    $stdin.gets.chomp
+  end
+end
